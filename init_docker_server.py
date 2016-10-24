2b9b6462e6a9e6486d204a9205ba37ae2b14daa2
Ray Pulsipher <rpulsipher@pencol.edu>
2016-10-24 15:42:26 -0700

    - Work on env variables for canvas - Start work on script to copy images into docker container - Add paramiko to project so we can send ssh commands and scp files over

---
+++ init_docker_server.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import os
+import paramiko
+from getpass import getpass
+
+# Intiailize git repos on the docker server
+
+# TODO - pull from env file?
+docker_host = "docker.pencollege.net"
+docker_user = "root"
+docker_password = ""
+ope_path = "/ope"
+
+if (docker_password == ""):
+    docker_password = getpass("Please enter Password for the docker server: ")
+docker_password = docker_password.strip()
+ope_path = ope_path.strip()
+
+# Setup inital ssh connection
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+try:
+    ssh.connect(docker_host, username=docker_user, password=docker_password)
+except Exception as e:
+    print "Error logging into " + docker_user + "@" + docker_host + "  --> " + str(e)
+    exit()
+# Open SFTP
+sftp = ssh.open_sftp()
+
+# Copy images
+pwd = os.getcwd()
+pwd = os.path.join(pwd, "images")
+for folder in os.listdir("./images/"):
+	#print folder
+    fname = os.path.basename(folder)
+    dpath = os.path.join(os.path.join(ope_path, "images"), fname).replace("\\", "/")
+    spath = os.path.join(pwd, fname).replace("/", "\\")
+    print "From: " + spath + " --> " + dpath
+    # Send the image
+    sftp.put(spath, dpath)
+    # Import it into the docker machines
+    stdin, stdout, stderr = ssh.exec_command("cd " + ope_path + "; docker load -i images/" fname)
+    print stdout
+    
+sftp.close()
+ssh.close()
--- ope-canvas/dbinit.sh
+++ ope-canvas/dbinit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-export POSTGRES_BIN=/usr/lib/postgresql/9.3/bin
+#export POSTGRES_BIN=/usr/lib/postgresql/9.3/bin
 
 # Use the user already setup
 #sudo -u postgres $POSTGRES_BIN/createuser --superuser canvas
 #sudo -u postgres $POSTGRES_BIN/createdb -E UTF-8 -T template0 --lc-collate=en_US.UTF-8 --lc-ctype=en_US.UTF-8 --owner canvas canvas_$RAILS_ENV
 #sudo -u postgres $POSTGRES_BIN/createdb -E UTF-8 -T template0 --lc-collate=en_US.UTF-8 --lc-ctype=en_US.UTF-8 --owner canvas canvas_queue_$RAILS_ENV
 
-export CANVAS_LMS_ADMIN_EMAIL="canvas@example.edu"
-export CANVAS_LMS_ADMIN_PASSWORD="canvas"
-export CANVAS_LMS_ACCOUNT_NAME="Canvas Docker"
+export CANVAS_LMS_ADMIN_EMAIL=$ADMIN_EMAIL
+export CANVAS_LMS_ADMIN_PASSWORD=$IT_PW
+export CANVAS_LMS_ACCOUNT_NAME=$LMS_ACCOUNT_NAME
 export CANVAS_LMS_STATS_COLLECTION="opt_out"
 
 cd /opt/canvas/canvas-lms \
