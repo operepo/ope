ccf3dd8e804953f659e73c4ba13f447fb6166b10
Ray Pulsipher <ray@cmagic.biz>
2016-10-25 06:40:35 -0700

    Work on switching canvas to production and runnning initial_setup/db_migrate on startup

--- ope-canvas/start.sh
+++ ope-canvas/start.sh
@@ -7,4 +7,33 @@ sed -i "s/SMTP_PORT/${SMTP_PORT-25}/" /opt/canvas/canvas-lms/config/outgoing_mai
 sed -i "s/SMTP_USER/${SMTP_USER-}/" /opt/canvas/canvas-lms/config/outgoing_mail.yml
 sed -i "s/SMTP_PASS/${SMTP_PASS-}/" /opt/canvas/canvas-lms/config/outgoing_mail.yml
 
+# Use the user already setup
+#sudo -u postgres $POSTGRES_BIN/createuser --superuser canvas
+#sudo -u postgres $POSTGRES_BIN/createdb -E UTF-8 -T template0 --lc-collate=en_US.UTF-8 --lc-ctype=en_US.UTF-8 --owner canvas canvas_$RAILS_ENV
+#sudo -u postgres $POSTGRES_BIN/createdb -E UTF-8 -T template0 --lc-collate=en_US.UTF-8 --lc-ctype=en_US.UTF-8 --owner canvas canvas_queue_$RAILS_ENV
+
+export CANVAS_LMS_ADMIN_EMAIL=$ADMIN_EMAIL
+export CANVAS_LMS_ADMIN_PASSWORD=$IT_PW
+export CANVAS_LMS_ACCOUNT_NAME=$LMS_ACCOUNT_NAME
+export CANVAS_LMS_STATS_COLLECTION="opt_out"
+
+cd /opt/canvas/canvas-lms
+
+cp config/domain.yml.tmpl config/domain.yml
+sed -i -- "s/<VIRTUAL_HOST>/$VIRTUAL_HOST/g" config/domain.yml
+sed -i -- "s/<IT_PW>/$IT_PW/g" config/database.yml
+
+$GEM_HOME/bin/bundle exec rake db:initial_setup
+
+$GEM_HOME/bin/bundle exec rake db:migrate
+
+$GEM_HOME/bin/bundle exec rails server
+# TODO - run db_migrate??
+#psql -U canvas -d canvas_development -c "INSERT INTO developer_keys (api_key, email, name, redirect_uri) VALUES ('test_developer_key', 'canvas@example.edu', 'Canvas Docker', 'http://localhost:8000');"
+
+# 'crypted_token' value is hmac sha1 of 'canvas-docker' using default config/security.yml encryption_key value as secret
+#psql -U canvas -d canvas_development -c "INSERT INTO access_tokens (created_at, crypted_token, developer_key_id, purpose, token_hint, updated_at, user_id) SELECT now(), '4bb5b288bb301d3d4a691ebff686fc67ad49daa8', dk.id, 'canvas-docker', '', now(), 1 FROM developer_keys dk where dk.email = 'canvas@example.edu';"
+
+
+
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
