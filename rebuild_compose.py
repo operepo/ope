ed4a7f3694e458254f0a06e481f728c1d7701760
Ray Pulsipher <ray@cmagic.biz>
2016-11-09 23:16:17 -0800

    - Add jsbin container - ensure .env file referenced in all containers -

--- rebuild_compose.py
+++ rebuild_compose.py
@@ -8,13 +8,13 @@ import shutil
 ## Write this to the docker-compose.yml file
 dc_out = """##### Open Prison Education - Docker Environment #####
 # NOTE - This file gets rebuilt, make changes to docker-compose-include.yml file
-# 		  in individual container directories and run rebuild_compose.py 
+#           in individual container directories and run rebuild_compose.py 
 #
 # Start docker containers by running this command from the main folder:
-#		docker-compose up -d
+#        docker-compose up -d
 #
 # Stop containers by running this command from the main folder:
-#		docker-compose down
+#        docker-compose down
 #
 # START OF docker-compose.yml
 version: '2'
@@ -32,47 +32,49 @@ replacement_values = { '<DOMAIN>': '', '<IP>': ''}
 
 # Find the local/public ip of the machine
 def getIP():
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	IP = '127.0.0.1'
-	try:
-		s.connect(('10.255.255.255', 0))
-		IP = s.getsockname()[0]
-	except:
-		IP = '127.0.0.1'
-	finally:
-		s.close()
-	return IP
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    IP = '127.0.0.1'
+    try:
+        s.connect(('10.255.255.255', 0))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 def processFolder(cwd=""):
-	ret = ""
-	if (os.path.isdir(cwd) != True):
-		#print "Not a folder, skipping..."
-		return ret
-	
-	enabled = os.path.join(cwd, ".enabled")
-	if (os.path.isfile(enabled) != True):
-		#print "Not enabled, skipping " + cwd
-		return ret
-	
-	print "Processing Folder " + cwd
-	
-	dc_import = os.path.join(cwd, "docker-compose-include.yml")
-	if (os.path.isfile(dc_import) != True):
-		print "\t\tSkipping - No docker-compose-include.yml file found"
-	
-	try:
-		f = open(dc_import, "r")
-		ret = f.read()
-		f.close()
-	except:
-		print "\t\t Error reading " + dc_import
-		ret = ""
-	
+    ret = ""
+    if (os.path.isdir(cwd) != True):
+        #print "Not a folder, skipping..."
+        return ret
+    
+    enabled = os.path.join(cwd, ".enabled")
+    if (os.path.isfile(enabled) != True):
+        #print "Not enabled, skipping " + cwd
+        return ret
+    
+    print "Processing Folder " + cwd
+    
+    dc_import = os.path.join(cwd, "docker-compose-include.yml")
+    if (os.path.isfile(dc_import) != True):
+        print "        Skipping - No docker-compose-include.yml file found"
+    
+    try:
+        f = open(dc_import, "r")
+        ret = f.read()
+        f.close()
+    except:
+        print "         Error reading " + dc_import
+        ret = ""
+    
+    # Make sure to add some line feeds to the end in case the this has tabs on
+    # the current line which messes up the yml format
+    ret += "\n\n"
+    return ret
 
-	return ret
 
-
-	
+    
 #print "Current IP: " + getIP()
 
 # Get current IP address
@@ -80,7 +82,7 @@ ip = getIP()
 choice = raw_input("Enter public IP [enter to use " + ip + "]: ")
 choice = choice.strip()
 if (choice != ""):
-	ip = choice
+    ip = choice
 replacement_values["<IP>"] = ip
 print "Using " + ip + "..."
 
@@ -88,7 +90,7 @@ domain = "ed"
 choice = raw_input("Enter domain to use [enter to use " + domain + "]: ")
 choice = choice.strip()
 if (choice != ""):
-	domain = choice
+    domain = choice
 replacement_values["<DOMAIN>"] = domain
 print "Using " + domain + "..."
 
@@ -98,44 +100,44 @@ pwd = os.getcwd()
 # Make sure the .env file exists
 env_file = os.path.join(pwd, ".env")
 if (os.path.isfile(env_file) != True):
-	# Try to copy the .env.template file
-	if (os.path.isfile(os.path.join(pwd, ".env.template")) == True):
-		print "\n\t\t\tNew environment file - change values in .env file\n"
-		shutil.copy(".env.template", ".env")
-	else:
-		print "No env file found! Create a .env file to store your settings"
+    # Try to copy the .env.template file
+    if (os.path.isfile(os.path.join(pwd, ".env.template")) == True):
+        print "\n            New environment file - change values in .env file\n"
+        shutil.copy(".env.template", ".env")
+    else:
+        print "No env file found! Create a .env file to store your settings"
 
 if (os.path.isfile(env_file) == True):
-	# Replace template tags with values from the replacement_values array
-	
-	# Read the current file in
-	env_f = open(env_file, "r")
-	lines = env_f.read()
-	env_f.close()
+    # Replace template tags with values from the replacement_values array
+    
+    # Read the current file in
+    env_f = open(env_file, "r")
+    lines = env_f.read()
+    env_f.close()
 
-	# Replace the values
-	for key in replacement_values:
-		lines = lines.replace(key, replacement_values[key])
-	
-	# Save the finished env file
-	env_f = open(env_file, "w")
-	env_f.write(lines)
-	env_f.close()
-		
+    # Replace the values
+    for key in replacement_values:
+        lines = lines.replace(key, replacement_values[key])
+    
+    # Save the finished env file
+    env_f = open(env_file, "w")
+    env_f.write(lines)
+    env_f.close()
+        
 # Loop through the folders and find containers with .enabled files.
 for folder in os.listdir("."):
-	dc_out += processFolder(os.path.join(pwd, folder))
+    dc_out += processFolder(os.path.join(pwd, folder))
 
 # Replace instances of template tags with values from the replacement_values array
 for key in replacement_values:
-	dc_out = dc_out.replace(key, replacement_values[key])
+    dc_out = dc_out.replace(key, replacement_values[key])
 
 # Clear the current docker-compose.yml file and write the new file
 docker_compose = open("docker-compose.yml", "w")
 docker_compose.write(dc_out)
-docker_compose.close()	
+docker_compose.close()    
 
-print "\n\nFinished!\n\n\tRun commands from main folder\n\t\tTo Build (Online Only): \tdocker-compose build\n\t\tTo start: \t\t\tdocker-compose up -d\n\t\tTo Stop: \t\t\tdocker-compose down"
+print "\n\nFinished!\n\n    Run commands from main folder\n        To Build (Online Only):     docker-compose build\n        To start:             docker-compose up -d\n        To Stop:             docker-compose down"
 # Grab all ope- folders and start each one
 
 #pwd = os.getcwd()
