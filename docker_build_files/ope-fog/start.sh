a0a5744fcdc68760ca573e23fa25a044702cc9ee
root <root@docker.ed>
2018-01-10 00:57:42 -0800

    - Fix canvas timing - create tables for auditing as seperate function from enabling auditing - Move Postgresql volume from volume to mounted folder (keeps it from destroying on docker-compose down) - Adjust network_mode to bridge on all docker compose template files (definable in rebuild_compose) - Adjustments to .fogsettings - Add ope-router - network mode host - allows adding iptables/etc... on startup

--- docker_build_files/ope-fog/start.sh
+++ docker_build_files/ope-fog/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# activate ICMP broadcast/multicast replise
+echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+
+ip link set lo multicast on
+ip link set eth0 multicast on
+
 # Make sure the .mntcheck files exist
 mkdir -p /images/dev
 touch /images/.mntcheck
@@ -8,26 +14,74 @@ touch /images/dev/.mntcheck
 
 
 # Start system services so nfs works
-service mountall.sh start
-/etc/init.d/rpcbind start
-/etc/init.d/nfs-common start
-/etc/init.d/nfs-kernel-server start
+echo "Setting NFS params..."
+#service mountall.sh start
+/etc/init.d/rpcbind restart
+/etc/init.d/nfs-common restart
+/etc/init.d/nfs-kernel-server restart
 
 
 # Make sure images folder is present
 mkdir -p /images/dev
 
 # Set permissions on folders
+echo "Changing permissions for /images, /tftpboot..."
 chown -R fog:root /images
 chmod -R 777 /images
 chown -R fog:root /tftpboot
 chmod -R 777 /tftpboot
 
-# Enable NAT features to allow tftp through
+WLAN_IF=eth0
+
+echo "Setting firewall rules..."
+# Enable NAT features to allow tftp through - off in host mode?
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
+# Rules to forward TFTP packets
+iptables -A INPUT -i $WLAN_IF -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -i $WLAN_IF -p udp --dport 69 -m state --state NEW -j ACCEPT
+
+# Allow multicast traffic through
+# Rules to forward multicast packets modprobe
+iptables -I INPUT -m pkttype --pkt-type multicast -j ACCEPT
+iptables -I INPUT -m pkttype --pkt-type broadcast -j ACCEPT
+iptables -I FORWARD -m pkttype --pkt-type multicast -j ACCEPT
+iptables -I OUTPUT -m pkttype --pkt-type multicast -j ACCEPT
+iptables -I INPUT -p udp --dport 12345 -j ACCEPT
+
+iptables -t mangle -A PREROUTING -d 224.0.0.0/4 -j TTL --ttl-set 32
+iptables -I INPUT -d 224.0.0.0/4 -j ACCEPT
+iptables -I INPUT -s 224.0.0.0/4 -j ACCEPT
+iptables -I FORWARD -s 224.0.0.0/4 -d 224.0.0.0/4 -j ACCEPT
+iptables -I OUTPUT  -d 224.0.0.0/4 -j ACCEPT
+
+# Need these if in host mode
+#iptables -A INPUT -p tcp --dport 7480 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 7443 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 20 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 21 -j ACCEPT
+#iptables -A INPUT -p udp --dport 69 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 2049 -j ACCEPT
+#iptables -A INPUT -p udp --dport 2049 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 111 -j ACCEPT
+#iptables -A INPUT -p udp --dport 111 -j ACCEPT
+#iptables -A INPUT -p udp --dport 4045 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 4045 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 34463 -j ACCEPT
+#iptables -A INPUT -p udp --dport 34463 -j ACCEPT
+#iptables -A INPUT -p udp --dport 9000:9008 -j ACCEPT
+#iptables -A INPUT -p udp --dport 7000:7030 -j ACCEPT
+#iptables -A INPUT -p udp --dport 43870 -j ACCEPT
+#iptables -A INPUT -p udp --dport 52764:52769 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 52764:52769 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 52700:52730 -j ACCEPT
+#iptables -A INPUT -p udp --dport 63100 -j ACCEPT
+
+
+
 #echo "Waiting for pipework..."
-#/bin/pipework --wait
+#/bin/pipework --wait -i $WLAN_IF
+#/bin/pipework eth0 $CONTAINERID dhclient
 
 # Make sure mysql is setup
 /usr/bin/mysql_install_db
@@ -38,47 +92,53 @@ then
     cp /.fogsettings /opt/fog/
 fi
 
-# Convert apache to use 7480 instead of 80 if running netowrk_mode: host
-#sed -i "s/:80/:7480/" /etc/apache2/sites-enabled/*.conf
-#sed -i "s/Listen 80/Listen 7480/" /etc/apache2/ports.conf
-# Convert apache to use 7443 instead of 443
-#sed -i "s/:443/:7443/" /etc/apache2/sites-enabled/*.conf
-#sed -i "s/Listen 443/Listen 7443/" /etc/apache2/ports.conf
-
-service mysql start
+echo "Starting mysql/apache..."
+service mysql restart
 service apache2 start
+#cp /001-fog.conf.orig /etc/apache2/sites-enabled/001-fog.conf 
+#service apache2 restart
+service xinetd start
 
 # Make sure the database scheme has been init
+echo "Updating fog database..."
 wget --no-check-certificate -qO - --post-data="confirm&fogverified" --no-proxy http://localhost/fog/management/index.php?node=schema
 
 # Make sure to update with the current public ip and password
+echo "Updating fog ip (update_fog_ip.py)..."
 cd /fog_src/bin
 python update_fog_ip.py
 bash update_password.sh
 
 # Make sure services are all up and ready
 #service mysql start
-service tftpd-hpa start
 # Make vsftpd warning not kill startup
-service vsftpd start || true
-service xinetd start
-service apache2 start
+#cp /001-fog.conf.orig /etc/apache2/sites-enabled/001-fog.conf 
+#service apache2 restart
 #service rpcbind start
 #service nfs-common start
 #service nfs-kernel-server start
 
-service FOGImageReplicator start
-service FOGImageSize start
-service FOGMulticastManager start
-service FOGPingHosts start
-service FOGScheduler start
-service FOGSnapinHash start
-service FOGSnapinReplicator start
+echo "Starting fog services..."
+service FOGImageReplicator restart
+service FOGImageSize restart
+service FOGMulticastManager restart
+service FOGPingHosts restart
+service FOGScheduler restart
+service FOGSnapinHash restart
+service FOGSnapinReplicator restart
+#service tftpd-hpa start  # started by xinetd
+service vsftpd start || true
+service pimd start
 
 #/bin/bash -c '. /etc/apache2/envvars; rm -Rf /var/run/apache2/*; /usr/sbin/apache2 -D FOREGROUND'
 
+# add nginx conf files so that fog virtual hosts appear
+# TODO 
+
+
 # Run forever so docker container doesn't quit
-sleep infinity
+trap : TERM INT; sleep infinity & wait
+
 #exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
 exit;
