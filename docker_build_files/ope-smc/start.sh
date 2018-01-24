9a1e1c12ac23a456c054699cd14b6c363946f6a1
Ray Pulsipher <ray@cmagic.biz>
2018-01-24 13:50:15 -0800

    - Make SMC app reset permissions on mounted folders on start and rebuild

--- docker_build_files/ope-smc/start.sh
+++ docker_build_files/ope-smc/start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #set -d
 
+# Make sure permissions are set on mounted folders
+chown -R www-data:www-data /home/www-data/web2py/applications/smc/cache
+chown -R www-data:www-data /home/www-data/web2py/applications/smc/databases
+chown -R www-data:www-data /home/www-data/web2py/applications/smc/errors
+chown -R www-data:www-data /home/www-data/web2py/applications/smc/private
+chown -R www-data:www-data /home/www-data/web2py/applications/smc/uploads
+
+
 # Start supervisord
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
