4debbc74ba84fcac236234b368b8dd3131da25cc
Ray Pulsipher <ray@cmagic.biz>
2018-11-19 00:35:48 -0800

    - Work on rebuild smc w web2py include - Add IS_ONLINE flag to apps

--- docker_build_files/ope-smc/start.sh
+++ docker_build_files/ope-smc/start.sh
@@ -2,16 +2,16 @@
 #set -d
 
 # Make sure permissions are set on mounted folders
-chown -R www-data:www-data /home/www-data/web2py/applications/smc/cache
-chown -R www-data:www-data /home/www-data/web2py/applications/smc/databases
-chown -R www-data:www-data /home/www-data/web2py/applications/smc/errors
-chown -R www-data:www-data /home/www-data/web2py/applications/smc/private
-chown -R www-data:www-data /home/www-data/web2py/applications/smc/uploads
-chown www-data:www-data /home/www-data/web2py/applications/smc/static/media
+chown -R www-data:www-data /home/www-data/smc/web2py/applications/smc/cache
+chown -R www-data:www-data /home/www-data/smc/web2py/applications/smc/databases
+chown -R www-data:www-data /home/www-data/smc/web2py/applications/smc/errors
+chown -R www-data:www-data /home/www-data/smc/web2py/applications/smc/private
+chown -R www-data:www-data /home/www-data/smc/web2py/applications/smc/uploads
+chown www-data:www-data /home/www-data/smc/web2py/applications/smc/static/media
 chown -R www-data:www-data /home/www-data/git
 
 # Make sure admin password is set
-cd /home/www-data/web2py
+cd /home/www-data/smc/web2py
 python -c "from gluon.main import save_password; save_password('$IT_PW',80)"
 python -c "from gluon.main import save_password; save_password('$IT_PW',443)"
 
