1a768e87881baf790665f0774245f46618567e43
Ray Pulsipher <ray@cmagic.biz>
2019-01-06 22:47:57 -0800

    .

--- client_tools/lms/src/build_me.cmd
+++ client_tools/lms/src/build_me.cmd
@@ -1,12 +1,14 @@
 
-set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\
+rem set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\
+set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\
+
 rem set VCToolsRedistDir="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Redist\MSVC\14.15.26706\"
 
 cd build-OPE_LMS-Desktop_Qt_5_11_2_MSVC2017_64bit-Release\release
 
-c:\Qt\5.11.2\msvc2017_64\bin\windeployqt.exe --compiler-runtime --angle OPE_LMS.exe
-
+c:\Qt\5.11.2\msvc2017_64\bin\windeployqt.exe --compiler-runtime --qmldir ../../OPE_LMS --angle OPE_LMS.exe
+rem C:\Qt\5.11.2\msvc2017_64\qml
 cd ..\..
 
-echo Done
+echo Done Building!!!!
 pause
