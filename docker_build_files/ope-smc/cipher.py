bebef9eb41ddf978324852d7998902885ef378e5
Ray Pulsipher <ray@cmagic.biz>
2023-08-30 12:20:02 -0700

    - Update SMC - pull cipher.py patch from alternate source for pytube.

--- docker_build_files/ope-smc/cipher.py
+++ docker_build_files/ope-smc/cipher.py
@@ -269,7 +269,7 @@ def get_throttling_function_name(js: str) -> str:
         # a.C && (b = a.get("n")) && (b = Bpa[0](b), a.set("n", b),
         # Bpa.length || iha("")) }};
         # In the above case, `iha` is the relevant function name
-        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&.*?\|\|\s*([a-z]+)',
+        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&\s*'
         r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
     ]
     logger.debug('Finding throttling function name')
@@ -284,7 +284,7 @@ def get_throttling_function_name(js: str) -> str:
             if idx:
                 idx = idx.strip("[]")
                 array = re.search(
-                    r'var {nfunc}\s*=\s*(\[.+?\])'.format(
+                    r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
                         nfunc=re.escape(function_match.group(1))),
                     js
                 )
